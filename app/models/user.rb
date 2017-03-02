bc6b4d6f33d799483aaa13dd70ff06920c0d873e
Sam Langenfeld <samlangenfeld@gmail.com>
2017-03-02 14:39:02 -0500

    Finished password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,8 +40,7 @@ class User < ApplicationRecord
 
 	def create_reset_digest
 		self.reset_token = User.new_token
-		update_attribute(:reset_digest, User.digest(reset_token))
-		update_attribute(:reset_sent_at, Time.zone.now)
+		self.update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
 	end
 
 	def send_password_reset_email
@@ -51,6 +50,10 @@ class User < ApplicationRecord
 
 	def send_activation_email
 		UserMailer.account_activation(self).deliver_now
+	end
+
+	def password_reset_expired?
+		reset_sent_at < 2.hours.ago
 	end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170302154429) do
+ActiveRecord::Schema.define(version: 20170302165343) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
